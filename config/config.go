2c7cdbd95a84c88a02ed73887e191dcef6f40987
Vinicius Rossado <vinirossado@gmail.com>
2023-07-15 23:44:11 +0300

    Fixed Url Base Template

--- config/config.go
+++ config/config.go
@@ -3,6 +3,6 @@ package config
 const (
 	WireCmd           = "github.com/google/wire/cmd/wire@latest"
 	GcliCmd           = "gcli"
-	RepoBase          = "https://github.com/vinirossado/DoYourLogin.git"
+	RepoBase          = "https://github.com/vinirossado/basic.git"
 	RepoFullStructure = "github.com/"
 )
