17e6740dc453b3d3d6191d48618acec579f6f920
Vinicius Rossado <vinirossado@gmail.com>
2024-02-09 15:55:26 +0200

    Cjheckpoint

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	WireCmd            = "github.com/google/wire/cmd/wire@latest"
-	GcliCmd            = "gcli"
+	GcliCmd            = "github.com/vinirossado/gcli/releases/tag/@latest"
 	RepoLiteStructure  = "https://github.com/vinirossado/gcli-lite-template.git"
 	RepoBasicStructure = "https://github.com/vinirossado/gcli-basic-template.git"
 	RepoFullStructure  = "https://github.com/vinirossado/gcli-advanced-template.git"
